8b65b231926c3559375e58a8c349e3a493ad0d2d
I C-AN <chicori3@naver.com>
2022-11-01 22:36:24 +0900

    Account 클래스를 static import 하도록 변경

--- src/main/java/java/io/buckpal/application/port/in/SendMoneyCommand.java
+++ src/main/java/java/io/buckpal/application/port/in/SendMoneyCommand.java
@@ -3,19 +3,19 @@ package java.io.buckpal.application.port.in;
 import lombok.Getter;
 
 import java.io.buckpal.common.SelfValidating;
-import java.io.buckpal.domain.Account;
 import java.io.buckpal.domain.Money;
 
+import static java.io.buckpal.domain.Account.AccountId;
 import static java.util.Objects.requireNonNull;
 
 @Getter
 public class SendMoneyCommand extends SelfValidating<SendMoneyCommand> {
 
-    private final Account.AccountId sourceAccountId;
-    private final Account.AccountId targetAccountId;
+    private final AccountId sourceAccountId;
+    private final AccountId targetAccountId;
     private final Money money;
 
-    public SendMoneyCommand(Account.AccountId sourceAccountId, Account.AccountId targetAccountId, Money money) {
+    public SendMoneyCommand(AccountId sourceAccountId, AccountId targetAccountId, Money money) {
         requireNonNull(sourceAccountId);
         requireNonNull(targetAccountId);
         this.validateSelf();
